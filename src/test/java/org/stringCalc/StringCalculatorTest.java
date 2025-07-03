22066e0a202a250012c1c777011183cb46f52965
VenuVirparia <venupatel004@gmail.com>
2025-07-03 18:06:05 +0530

    [Step 1] Red: test - return sum for two numbers string

--- src/test/java/org/stringCalc/StringCalculatorTest.java
+++ src/test/java/org/stringCalc/StringCalculatorTest.java
@@ -19,5 +19,8 @@ class StringCalculatorTest {
     void return_number_on_single_number_string() {
         assertEquals(4, stringCalculator.add("4"));
     }
-
+    @Test
+    void return_sum_on_two_numbers_string() {
+        assertEquals(16, stringCalculator.add("14,2"));
+    }
 }
