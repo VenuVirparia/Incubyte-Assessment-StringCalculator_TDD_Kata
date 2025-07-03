d9988482642234dff147d858f9aac0c47feffee0
VenuVirparia <venupatel004@gmail.com>
2025-07-03 17:56:55 +0530

    [Step 1] Green: add() - return number for single number string

--- src/main/java/org/stringCalc/StringCalculator.java
+++ src/main/java/org/stringCalc/StringCalculator.java
@@ -5,6 +5,7 @@ public class StringCalculator {
     public int add(String numbers) {
         if(numbers == null || numbers.isEmpty())
             return 0;
-        return 0;
+        else
+            return Integer.parseInt(numbers);
     }
 }
