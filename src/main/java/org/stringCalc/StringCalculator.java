8cc816b40c98a8e041cbc5e867386ea226a7dfdb
VenuVirparia <venupatel004@gmail.com>
2025-07-03 19:26:49 +0530

    [Step 2] Refactor: add(), sumNumbers() - Separate out sum logic for better readability

--- src/main/java/org/stringCalc/StringCalculator.java
+++ src/main/java/org/stringCalc/StringCalculator.java
@@ -2,19 +2,21 @@ package org.stringCalc;
 
 public class StringCalculator {
 
+    private int sumNumbers(String[] numberArray) {
+        int sum = 0;
+        for (String number : numberArray) {
+            sum += Integer.parseInt(number);
+        }
+        return sum;
+    }
+
     public int add(String numbers) {
         if(numbers == null || numbers.isEmpty())
             return 0;
-        else if(numbers.contains(","))
-        {
-            String[] numberArray = numbers.split(",");
-            int sum = 0;
-            for(String number : numberArray){
-                sum += Integer.parseInt(number);
-            }
-        return sum;
-        }
-        else
-            return Integer.parseInt(numbers);
+
+        if(numbers.contains(","))
+            return sumNumbers(numbers.split(","));
+
+        return Integer.parseInt(numbers);
     }
 }
