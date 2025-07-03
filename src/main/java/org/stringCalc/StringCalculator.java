3ba2b7b3b97147701a9387942bf2feb0d40f01bb
VenuVirparia <venupatel004@gmail.com>
2025-07-03 18:11:42 +0530

    [Step 1] Green: add() - return sum for two numbers string

--- src/main/java/org/stringCalc/StringCalculator.java
+++ src/main/java/org/stringCalc/StringCalculator.java
@@ -5,6 +5,11 @@ public class StringCalculator {
     public int add(String numbers) {
         if(numbers == null || numbers.isEmpty())
             return 0;
+        else if(numbers.contains(","))
+        {
+            String[] numberArray = numbers.split(",");
+            return Integer.parseInt(numberArray[0]) + Integer.parseInt(numberArray[1]);
+        }
         else
             return Integer.parseInt(numbers);
     }
