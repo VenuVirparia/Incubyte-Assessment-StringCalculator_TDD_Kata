11e8917d1e16dfbf6118a12eb7cc7beb47c207e0
VenuVirparia <venupatel004@gmail.com>
2025-07-03 17:27:06 +0530

    [Step 1] Refactor: add() - return 0 for null or empty string input

--- src/main/java/org/stringCalc/StringCalculator.java
+++ src/main/java/org/stringCalc/StringCalculator.java
@@ -3,6 +3,8 @@ package org.stringCalc;
 public class StringCalculator {
 
     public int add(String numbers) {
+        if(numbers == null || numbers.isEmpty())
+            return 0;
         return 0;
     }
 }
