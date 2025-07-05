40674b716842837887d81c7ea58961323366f3a3
VenuVirparia <venupatel004@gmail.com>
2025-07-05 19:36:22 +0530

    [Step 8] Refactor: getCustomDelimitersIfExists(), buildRegex(), add() - refactor functions for better readability

--- src/main/java/org/stringCalc/StringCalculator.java
+++ src/main/java/org/stringCalc/StringCalculator.java
@@ -9,11 +9,15 @@ public class StringCalculator {
     private int sumNumbers(String[] numberArray) throws IllegalArgumentException {
         int sum = 0;
         List<String> negativeNumbers = new ArrayList<>();
+
         for (String number : numberArray) {
             int currentNumber = Integer.parseInt(number);
+
             if (currentNumber < 0)
                 negativeNumbers.add(currentNumber+"");
+
             if (currentNumber > 1000) continue;
+
             sum += currentNumber;
         }
         if (!negativeNumbers.isEmpty())
@@ -25,14 +29,12 @@ public class StringCalculator {
         List<String> delimiters = new ArrayList<>();
 
         if (numbers.startsWith("//[")) {
-            int start = numbers.indexOf("//") + 2;
-            int end = numbers.indexOf("\n");
-            String delimiterSection = numbers.substring(start, end);
+            String delimiterSection = numbers.substring(numbers.indexOf("//") + 2, numbers.indexOf("\n"));
 
             while (delimiterSection.contains("[")) {
-                int open = delimiterSection.indexOf("[");
+                int open = delimiterSection.indexOf("[") + 1;
                 int close = delimiterSection.indexOf("]");
-                delimiters.add(delimiterSection.substring(open + 1, close));
+                delimiters.add(delimiterSection.substring(open, close));
                 delimiterSection = delimiterSection.substring(close + 1);
             }
         } else if (numbers.startsWith("//")) {
@@ -50,10 +52,12 @@ public class StringCalculator {
     String buildRegex(List<String> delimiters) {
         delimiters.add(",");
         delimiters.add("\n");
+
         List<String> quoted = new ArrayList<>();
         for (String d : delimiters) {
             quoted.add(Pattern.quote(d));
         }
+
         return String.join("|", quoted);
     }
 
